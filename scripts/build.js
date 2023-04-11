ceb54ba74f3e8820aa9b31fffb8a80d6bf404083
kevinfiol <me@kevinfiol.com>
2023-04-10 22:51:39 -0400

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,70 +1,69 @@
-import servbot from 'servbot';
 import esbuild from 'esbuild';
+import servbot from 'servbot';
 import env from 'env-smart';
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 
 // load .env file
 env.load();
 
-const DEV = process.argv.includes('-d');
+const PORT = 8081;
+const DEV = process.argv.includes('--dev');
 const API_URL = process.env.API_URL;
-const ENTRY = resolve('src/index.js');
 const OUTFILE = resolve('dist/app.js');
-const SERVER_PORT = 8081;
+const ENTRY = resolve('src/index.js');
 
-function logSuccess() {
-    console.log('\x1b[42m%s\x1b[0m', `Bundled: ${OUTFILE}`);
-}
+/** @type {import('servbot').ServbotServer?} **/
+let server;
 
-function logError(msg) {
-    console.error('\x1b[41m%s\x1b[0m', msg)
-}
+/** @type {esbuild.BuildOptions} **/
+const esbuildConfig = {
+  format: 'iife',
+  entryPoints: [ENTRY],
+  outfile: OUTFILE,
+  bundle: true,
+  minify: !DEV,
+  sourcemap: DEV,
+  jsxFactory: 'm',
+  jsxFragment: '"["',
+  define: {
+    'process.env.API_URL': `"${API_URL}"`,
+    'window.DEV_MODE': DEV ? 'true' : 'false'
+  },
+  plugins: [{
+    name: 'on-end',
+    setup(build) {
+      build.onEnd(({ errors }) => {
+        if (errors[0]) {
+          console.error('Bundling Failed!', errors[0]);
+          return;
+        }
 
-function bundle(config = {}) {
-    return esbuild.build({
-        minify: true,
-        format: 'esm',
-        entryPoints: [ENTRY],
-        outfile: OUTFILE,
-        bundle: true,
-        jsxFactory: 'm',
-        jsxFragment: '"["',
-        define: {
-            'process.env.API_URL': `"${API_URL}"`
-        },
-        ...config
-    });
-}
+        console.log('Bundled: ', OUTFILE);
+        if (server) server.reload();
+      });
+    }
+  }]
+};
 
-let config = {};
+(async () => {
+  // create & configure context
+  const ctx = await esbuild.context(esbuildConfig);
 
-if (DEV) {
-    const server = servbot({
+  if (DEV) {
+    server = servbot({
       root: 'dist',
       reload: true,
       fallback: 'index.html'
     });
 
-    server.listen(SERVER_PORT);
+    server.listen(PORT);
+    await ctx.watch();
 
-    config = {
-        minify: false,
-        sourcemap: true,
-        watch: {
-            onRebuild(error) {
-                if (error) logError(error);
-                else {
-                    logSuccess();
-                    server.reload();
-                }
-            }
-        }
-    };
-}
-
-bundle(config)
-    .then(logSuccess)
-    .catch((error) => {
-        logError(error);
-        process.exit(1);
+    process.on('exit', () => {
+      ctx.dispose();
+      server.close();
     });
+  } else {
+    ctx.rebuild().finally(ctx.dispose);
+  }
+})();
